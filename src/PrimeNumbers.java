36df45a588383cae4b13a4d4f189a8b578a8f4d1
Admin <jaisonotalvaro@gmail.com>
2023-02-14 01:19:19 +0100

    Septimo Commit: cambio los nombres a los métodos para que sean más descriptivos, los ordeno según se usan en el método principal.

--- src/PrimeNumbers.java
+++ src/PrimeNumbers.java
@@ -6,16 +6,34 @@ public class PrimeNumbers {
     public static int[] generarPrimos (int max) {
         int i,j;
         if (max >= 2) {
-            esPrimo = getBooleans(max);
-            extracted();
-            primos = getInts();
+            esPrimo = recorrerTabla(max);
+            descartarNumerosMultiplos();
+            primos = contarRellenar();
             return primos;
         } else {
             return new int[0];
         }
     }
-
-    private static int[] getInts() {
+    private static boolean[] recorrerTabla(int max) {
+        int i;
+        esPrimo = new boolean[max + 1];
+        for (i=0; i< esPrimo.length; i++)
+            esPrimo[i] = true;
+        esPrimo[0] = false;
+        esPrimo[1] = false;
+        return esPrimo;
+    }
+    private static void descartarNumerosMultiplos() {
+        int i;
+        int j;
+        for (i=2; i<Math.sqrt(esPrimo.length)+1; i++) {
+            if (esPrimo[i]) {
+                for (j=2*i; j< esPrimo.length; j+=i)
+                    esPrimo[j] = false;
+            }
+        }
+    }
+    private static int[] contarRellenar() {
         int i;
         int j;
         int cuenta = 0;
@@ -31,26 +49,6 @@ public class PrimeNumbers {
         return primos;
     }
 
-    private static void extracted() {
-        int i;
-        int j;
-        for (i=2; i<Math.sqrt(esPrimo.length)+1; i++) {
-            if (esPrimo[i]) {
-                for (j=2*i; j< esPrimo.length; j+=i)
-                    esPrimo[j] = false;
-            }
-        }
-    }
-
-    private static boolean[] getBooleans(int max) {
-        int i;
-        esPrimo = new boolean[max + 1];
-        for (i=0; i< esPrimo.length; i++)
-            esPrimo[i] = true;
-        esPrimo[0] = false;
-        esPrimo[1] = false;
-        return esPrimo;
-    }
 
     public static void main(String[] args) {
         Scanner teclado=new Scanner(System.in);
